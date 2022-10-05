f73f0b6680271c4087c66b40640551d6946beb2b
ksenia-eremenko <ksushaa19@gmail.com>
2022-10-05 18:25:27 +0300

    test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
-function stringToArray(string){
-
-	return string.split(' ');
-
+function expressionMatter(a, b, c) {
+  let result1 = a * (b + c);
+  let result2 = a * b * c;
+  let result3 = a + b * c;
+  let result4 = (a + b) * c;
+  return Math.max(result1, result2, result3, result4);
 }
-console.log(stringToArray("qw wq"))
+console.log(expressionMatter(1,2,3));
